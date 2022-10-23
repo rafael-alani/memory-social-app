e7fcf10c641f4a52680b41fbfa039c7be6d98f43
rafael-alani <rafael.alani@hotmail.com>
2022-10-23 17:46:53 +0200

    create post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 const router = express.Router();
+
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
@@ -16,14 +17,20 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const post = req.body;
+  const { title, message, selectedFile, creator, tags } = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
 
   try {
-    await newPost().save();
+    await newPostMessage.save();
 
-    res.status(201).json(newPost);
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://rafael:MainMid123@cluster0.ou2zfqx.mongodb.net/?retryWrites=true&w=majority";
+//mongodb+srv://rafael:<password>@cluster0.ou2zfqx.mongodb.net/?retryWrites=true&w=majority
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -23,5 +24,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error));
-
-mongoose.set("useFindAndModify", false);
